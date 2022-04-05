5b571de4f7c58ccb4b7cc9af5853e4ea60c260fd
Andrew <armcburn@gmail.com>
2022-04-04 20:58:47 -0400

    Add Redux, Axios

---
+++ client/src/actions/posts.js
@@ -0,0 +1,10 @@
+import * as api from "../api";
+
+// Action Creators: Are functions that return actions
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {}
+};
